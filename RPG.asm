71b736b4217c055dad64656ba2bb76af71f1dd75
Skeome <skeome@gmail.com>
2025-11-05 04:07:41 -0800

    Updated developer info in RPG.ASM

---
+++ RPG.asm
@@ -0,0 +1,7 @@
+;-------------------------------------------------------
+; Assembly Program: RPG                                 |
+; Target Platform: Linux x86_64                         |   
+; Description: A classic RPG game                       |
+; Assembler: NASM                                       |
+; Author: Skeome                                        |
+;-------------------------------------------------------
